fb15f76be93870c506785f3358db82da628897bc
Céleste Wouters <foss@elementw.net>
2026-04-18 14:24:43 +0200

    Start: use BufferStreambuf instead of StringOStreambuf

--- src/Mod/Start/App/FcstdInfoSource.cpp
+++ src/Mod/Start/App/FcstdInfoSource.cpp
@@ -58,20 +58,14 @@ static QByteArray loadFCStdThumbnail(const App::ProjectFile& proj, const QString
 
                 // Read the thumbnail into a buffer
                 const auto dataSize = proj.sizeOfFile(pathToThumbnail);
-#if __cplusplus >= 202302L
                 QByteArray data(dataSize, Qt::Uninitialized);
+#if __cplusplus >= 202302L
                 std::spanstream dataStream({data.data(), size_t(data.size())});
 #else
-                std::string dataString;
-                dataString.reserve(dataSize);
-                Base::StringOStreambuf dataStreambuf(dataString);
+                Base::BufferStreambuf dataStreambuf({data.data(), size_t(data.size())});
                 std::ostream dataStream(&dataStreambuf);
 #endif
                 proj.readInputFileDirect(pathToThumbnail, dataStream);
-#if __cplusplus < 202302L
-                // Incurs a copy
-                QByteArray data(dataString.data(), qsizetype(dataString.size()));
-#endif
 
                 // Save that buffer to the thumbnail cache
                 const Base::FileInfo thumbnailFileInfo(pathToCachedThumbnail.toStdString());
