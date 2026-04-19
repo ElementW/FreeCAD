fe30727bd0d468a87403b0430be0129963df0cf3
Céleste Wouters <foss@elementw.net>
2026-04-19 23:21:39 +0200

    Start: CustomFolderModel: use std::filesystem

--- src/Mod/Start/App/CustomFolderModel.cpp
+++ src/Mod/Start/App/CustomFolderModel.cpp
@@ -1,33 +1,36 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-/****************************************************************************
- *                                                                          *
- *   Copyright (c) 2024 The FreeCAD Project Association AISBL               *
- *                                                                          *
- *   This file is part of FreeCAD.                                          *
- *                                                                          *
- *   FreeCAD is free software: you can redistribute it and/or modify it     *
- *   under the terms of the GNU Lesser General Public License as            *
- *   published by the Free Software Foundation, either version 2.1 of the   *
- *   License, or (at your option) any later version.                        *
- *                                                                          *
- *   FreeCAD is distributed in the hope that it will be useful, but         *
- *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
- *   Lesser General Public License for more details.                        *
- *                                                                          *
- *   You should have received a copy of the GNU Lesser General Public       *
- *   License along with FreeCAD. If not, see                                *
- *   <https://www.gnu.org/licenses/>.                                       *
- *                                                                          *
- ***************************************************************************/
+// SPDX-FileCopyrightText: 2026 Céleste Wouters <foss@elementw.net>
+// SPDX-FileNotice: Part of the FreeCAD project.
 
-#include <QDir>
+/******************************************************************************
+ *                                                                            *
+ *   FreeCAD is free software: you can redistribute it and/or modify          *
+ *   it under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1            *
+ *   of the License, or (at your option) any later version.                   *
+ *                                                                            *
+ *   FreeCAD is distributed in the hope that it will be useful,               *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty              *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *   See the GNU Lesser General Public License for more details.              *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public         *
+ *   License along with FreeCAD. If not, see https://www.gnu.org/licenses     *
+ *                                                                            *
+ ******************************************************************************/
 
 #include "CustomFolderModel.h"
+
+#include <filesystem>
+#include <ranges>
+#include <string_view>
+
 #include <App/Application.h>
-#include <QStringList>
+
 
 using namespace Start;
+using namespace std::string_view_literals;
+namespace fs = std::filesystem;
 
 
 CustomFolderModel::CustomFolderModel(QObject* parent, int thumbnailSizeHint)
@@ -38,9 +41,9 @@ CustomFolderModel::CustomFolderModel(QObject* parent, int thumbnailSizeHint)
         "User parameter:BaseApp/Preferences/Mod/Start"
     );
 
-    _customFolderPathSpec = QString::fromStdString(parameterGroup->GetASCII("CustomFolder", ""));
+    customFolderPathSpec = parameterGroup->GetASCII("CustomFolder", "");
 
-    _showOnlyFCStd = parameterGroup->GetBool("ShowOnlyFCStd", false);
+    showOnlyFCStd = parameterGroup->GetBool("ShowOnlyFCStd", false);
 }
 
 /// If the custom folder path contains multiple paths separated by ';;', split them into individual
@@ -51,35 +54,29 @@ void CustomFolderModel::loadCustomFolder()
 {
     beginResetModel();
     clear();
-    auto pathDelimiter = QStringLiteral(";;");
-    auto paths = _customFolderPathSpec.split(pathDelimiter, Qt::SkipEmptyParts);
+    constexpr auto pathDelimiter = ";;"sv;
 
-    for (const auto& path : paths) {
-        QDir customFolderDirectory(path);
-        if (!customFolderDirectory.exists()) {
+    std::vector<fs::directory_entry> entries;
+    for (const auto path : std::views::split(customFolderPathSpec, pathDelimiter)) {
+        const fs::path customFolderDirectory(path.begin(), path.end());
+        entries.clear();
+        try {
+            for (auto file : std::filesystem::directory_iterator {customFolderDirectory}) {
+                if (!showOnlyFCStd || file.path().extension().compare(".FCStd"sv) == 0) {
+                    entries.emplace_back(std::move(file));
+                }
+            }
+        }
+        catch (const std::filesystem::filesystem_error& e) {
             Base::Console().warning(
-                "BaseApp/Preferences/Mod/Start/CustomFolder: custom folder %s does not exist\n",
-                customFolderDirectory.absolutePath().toStdString().c_str()
+                "BaseApp/Preferences/Mod/Start/CustomFolder: cannot read custom folder %s: %s\n",
+                customFolderDirectory.string(),
+                e.code().message()
             );
-            continue;
         }
-        if (!customFolderDirectory.isReadable()) {
-            Base::Console().warning(
-                "BaseApp/Preferences/Mod/Start/CustomFolder: cannot read custom folder %s\n",
-                customFolderDirectory.absolutePath().toStdString().c_str()
-            );
-            continue;
-        }
-        if (_showOnlyFCStd) {
-            customFolderDirectory.setNameFilters(QStringList() << QStringLiteral("*.FCStd"));
-        }
-
-        auto entries = customFolderDirectory.entryList(
-            QDir::Filter::Files | QDir::Filter::Readable,
-            QDir::SortFlag::Name
-        );
+        std::ranges::sort(entries);
         for (const auto& entry : entries) {
-            addFile(customFolderDirectory.filePath(entry));
+            addFile(QString::fromStdString(entry.path().string()));
         }
     }
 
--- src/Mod/Start/App/CustomFolderModel.h
+++ src/Mod/Start/App/CustomFolderModel.h
@@ -1,34 +1,31 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
-/****************************************************************************
- *                                                                          *
- *   Copyright (c) 2024 The FreeCAD Project Association AISBL               *
- *                                                                          *
- *   This file is part of FreeCAD.                                          *
- *                                                                          *
- *   FreeCAD is free software: you can redistribute it and/or modify it     *
- *   under the terms of the GNU Lesser General Public License as            *
- *   published by the Free Software Foundation, either version 2.1 of the   *
- *   License, or (at your option) any later version.                        *
- *                                                                          *
- *   FreeCAD is distributed in the hope that it will be useful, but         *
- *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
- *   Lesser General Public License for more details.                        *
- *                                                                          *
- *   You should have received a copy of the GNU Lesser General Public       *
- *   License along with FreeCAD. If not, see                                *
- *   <https://www.gnu.org/licenses/>.                                       *
- *                                                                          *
- ***************************************************************************/
+// SPDX-FileCopyrightText: 2026 FreeCAD contributors
+// SPDX-FileNotice: Part of the FreeCAD project.
+
+/******************************************************************************
+ *                                                                            *
+ *   FreeCAD is free software: you can redistribute it and/or modify          *
+ *   it under the terms of the GNU Lesser General Public License as           *
+ *   published by the Free Software Foundation, either version 2.1            *
+ *   of the License, or (at your option) any later version.                   *
+ *                                                                            *
+ *   FreeCAD is distributed in the hope that it will be useful,               *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty              *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *   See the GNU Lesser General Public License for more details.              *
+ *                                                                            *
+ *   You should have received a copy of the GNU Lesser General Public         *
+ *   License along with FreeCAD. If not, see https://www.gnu.org/licenses     *
+ *                                                                            *
+ ******************************************************************************/
 
 #pragma once
 
-#include <QAbstractListModel>
-#include <QString>
-#include <Base/Parameter.h>
+#include "../StartGlobal.h"
+
+#include <string>
 
 #include "DisplayedFilesModel.h"
-#include "../StartGlobal.h"
 
 namespace Start
 {
@@ -44,8 +41,8 @@ public:
     void loadCustomFolder();
 
 private:
-    QString _customFolderPathSpec;
-    bool _showOnlyFCStd;  // Show only FreeCAD files
+    std::string customFolderPathSpec;
+    bool showOnlyFCStd;  // Show only FreeCAD files
 };
 
 }  // namespace Start
