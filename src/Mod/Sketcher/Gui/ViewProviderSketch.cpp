ElementW/FreeCAD

commit 16a836743a4b60ef919e5f003eb1427cdb766d19

Sketcher: Fix updating a spreadsheet dimension does not update an open sketch 17036

src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -3895,15 +3895,8 @@ void ViewProviderSketch::slotSolverUpdate()
             + getSketchObject()->getHighestCurveIndex() + 1
         == getSolvedSketch().getGeometrySize()) {
 
-        auto editDoc = Gui::Application::Instance->editDocument([this](Gui::Document* editdoc) {
-            return editdoc->getEditViewProvider() == this;
-        });
-
-        Gui::MDIView* mdi = nullptr;
-
-        if (editDoc) {
-            mdi = editDoc->getActiveView();
-        }
+        Gui::MDIView* mdi =
+            Gui::Application::Instance->editViewOfNode(editCoinManager->getRootEditNode());
         if (mdi && mdi->isDerivedFrom<Gui::View3DInventor>()) {
             draw(false, true);
         }

commit 13b07d70e6e27ee61f8894531c22aeeb46aa93ed

Sketcher: fix #32635 crash on mouse move for some sketches
Fixes a null deref caused by 16a836743a4b60ef919e5f003eb1427cdb766d19
improperly launching sketch edit mode, meaning
EditModeGeometryCoinConverter::convert() ended up not being called and
not populating sketcher Coin editor data.

src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -3899,8 +3899,18 @@ void ViewProviderSketch::slotSolverUpdate()
             + getSketchObject()->getHighestCurveIndex() + 1
         == getSolvedSketch().getGeometrySize()) {
 
-        Gui::MDIView* mdi =
-            Gui::Application::Instance->editViewOfNode(editCoinManager->getRootEditNode());
+        Gui::MDIView* mdi = Gui::Application::Instance->editViewOfNode(
+            editCoinManager->getRootEditNode()
+        );
+
+        if (mdi == nullptr) {
+            if (auto editDoc = Gui::Application::Instance->editDocument(
+                    [this](Gui::Document* editdoc) { return editdoc->getEditViewProvider() == this; }
+                )) {
+                mdi = editDoc->getActiveView();
+            }
+        }
+
         if (mdi && mdi->isDerivedFrom<Gui::View3DInventor>()) {
             draw(false, true);
         }